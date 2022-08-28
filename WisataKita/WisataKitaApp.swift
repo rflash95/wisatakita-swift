8f413f26ac9e2a5e66ede21cbdec292065947348
Reza Maulana <rflash95@gmail.com>
2022-08-28 21:33:24 +0700

    Initial Commit

---
+++ WisataKita/WisataKitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WisataKitaApp.swift
+//  WisataKita
+//
+//  Created by rzamau on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct WisataKitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
